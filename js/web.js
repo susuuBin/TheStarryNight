88a5b5fa666637eaa0197163c74c2810229cd8dd
susuuBin <azxq1000@gmail.com>
2020-04-20 18:23:31 +0900

    index 파일 경로 수정 및 보완

--- js/web.js
+++
@@ -1,9 +0,0 @@
-function isMobile(){ //모바일 여부
-    var uAgent = navigator.userAgent.toLowerCase();
-
-    var mobilePhones = new Array('iphone', 'ipod', 'ipad', 'android', 'blackberry', 'windows ce', 'nokia', 'webos', 'opera mini', 'sonyericsson', 'opera mobi', 'iemobile');
-    for(var i = 0; i < mobilePhones.length; i++){
-        if(uAgent.indexOf(mobilePhones[i]) != -1)
-            document.location = "아직임"
-    }
-}
