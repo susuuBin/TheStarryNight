bfb3a8a960875064de96c5f2256c457bf5b7b0d8
pcnim02 <s2018w13@e-mirim.hs.kr>
2020-04-27 00:32:40 +0900

    add

--- js/goartgallery.js
+++ js/goartgallery.js
@@ -1,20 +1,60 @@
-var imgArray = new Array();
-        imgArray[0] = "images/light/light.png";
+var imgArray1 = new Array();
+        imgArray1[0] = "images/light/light.png";
 
- 
         function showImage() {
-            var imgNum = Math.round(Math.random()*2);
-            var objImg = document.getElementById("light1");
-            objImg.src = imgArray[imgNum];
-            setTimeout("showImage()", 1000);
+            var objImg1 = document.getElementById("light1");
+            objImg1.src = imgArray1[0];
         }
-var imgArray = new Array();
-        imgArray[0] = "images/light/light1.png";
 
+        window.setTimeout(showImage(), 50000);
+
+var imgArray2 = new Array();
+        imgArray2[0] = "images/light/light1.png";
  
-        function showImage() {
-            var imgNum = Math.round(Math.random()*2);
-            var objImg = document.getElementById("light2");
-            objImg.src = imgArray[imgNum];
-            setTimeout("showImage()", 1000);
+        function showImage2() {
+            var objImg2 = document.getElementById("light2");
+            objImg2.src = imgArray2[0];
         }
+
+        window.setTimeout(showImage2(), 100000);
+
+var imgArray3 = new Array();
+        imgArray3[0] = "images/light/light3.png";
+ 
+        function showImage3() {
+            var objImg3 = document.getElementById("light3");
+            objImg3.src = imgArray3[0];
+        }
+
+        window.setTimeout(showImage3(), 150000);
+
+var imgArray4 = new Array();
+        imgArray4[0] = "images/light/light4.png";
+ 
+        function showImage4() {
+            var objImg4 = document.getElementById("light4");
+            objImg4.src = imgArray4[0];
+        }
+
+        window.setTimeout(showImage4(), 200000);
+
+var imgArray5 = new Array();
+        imgArray5[0] = "images/light/light5.png";
+ 
+        function showImage5() {
+            var objImg5 = document.getElementById("light5");
+            objImg5.src = imgArray5[0];
+        }
+
+        window.setTimeout(showImage5(), 200000);
+
+
+var imgArray6 = new Array();
+        imgArray6[0] = "images/light/light6.png";
+ 
+        function showImage6() {
+            var objImg6 = document.getElementById("light6");
+            objImg6.src = imgArray6[0];
+        }
+
+        window.setTimeout(showImage6(), 200000);
